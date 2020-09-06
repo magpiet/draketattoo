96a11a37341a5d5d20c1e309264e1a27f7fe72a3
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-09-06 17:06:05 -0500

    Updates

---
+++ precache-manifest.5edd24cd22a84b0bf0c4fe5a1155d10d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01bb509adde5154c06ee07c67c8bc85",
+    "url": "/draketattoo/index.html"
+  },
+  {
+    "revision": "18ad0056985789fadd66",
+    "url": "/draketattoo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c3745657c8fc9a1b3b8",
+    "url": "/draketattoo/static/js/2.b513ef82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/draketattoo/static/js/2.b513ef82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ad0056985789fadd66",
+    "url": "/draketattoo/static/js/main.e01b63e0.chunk.js"
+  },
+  {
+    "revision": "09474e43a5647e9f7f98",
+    "url": "/draketattoo/static/js/runtime-main.d69c274d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/draketattoo/static/media/logo.5d5d9eef.svg"
+  }
+]);
